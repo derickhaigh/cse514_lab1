b81029cdf2a9fbedcfb74a62a586404f7b3b151d
derickhaigh <derickhaigh@live.com>
2021-03-21 14:57:42 -0400

    Added getaddrinfo to server code

--- server/P2P_server.cpp
+++ server/P2P_server.cpp
@@ -6,10 +6,21 @@
 #include <string.h>
 
 #define PORT 8080
+#define BUFF_SIZE 1024
 
 int requestHandler(int client_fd);
 
 int main(int argc, char* argv[]){
+
+    //Stuff for getaddrinfo
+    struct addrinfo hints;
+    struct addrinfo *result, *rp;
+    int sfd, s;
+    struct sockaddr_storage peer_addr;
+    socklen_t peer_addr_len;
+    ssize_t nread;
+    char buf[BUFF_SIZE];
+
 
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
@@ -18,6 +29,48 @@ int main(int argc, char* argv[]){
     char buffer[1024] = {0};
     char *hello = "Hello from server";
 
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s port\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;    /* Allow IPv4 or IPv6 */
+    hints.ai_socktype = SOCK_STREAM; /* Datagram socket */
+    hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
+    hints.ai_protocol = 0;          /* Any protocol */
+    hints.ai_canonname = NULL;
+    hints.ai_addr = NULL;
+    hints.ai_next = NULL;
+
+    s = getaddrinfo(NULL, argv[1], &hints, &result);
+    if (s != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
+        exit(EXIT_FAILURE);
+    }
+
+
+    for (rp = result; rp != NULL; rp = rp->ai_next) {
+        sfd = socket(rp->ai_family, rp->ai_socktype,
+                rp->ai_protocol);
+        if (sfd == -1)
+            continue;
+
+        if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0)
+            break;                  /* Success */
+
+        close(sfd);
+    }
+
+    freeaddrinfo(result);           /* No longer needed */
+
+    if (rp == NULL) {               /* No address succeeded */
+        fprintf(stderr, "Could not bind\n");
+        exit(EXIT_FAILURE);
+    }
+
+/*
     //Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM,0)) == 0){
         perror("Socket failed");
@@ -38,7 +91,7 @@ int main(int argc, char* argv[]){
     if(bind(server_fd, (struct sockaddr *)&address,sizeof(address))<0){
         perror("Bind Failed");
         exit(EXIT_FAILURE);
-    }
+    }*/
 
     //Start to listen
     if(listen(server_fd, 3)<0){
