6caffc3a509862c8c1e60e72696924cf3791fe05
derickhaigh <derickhaigh@live.com>
2021-03-22 23:18:50 -0400

    Trying to add epoll functionality to queue and handle messages from multiple clients

--- server/P2P_server.cpp
+++ server/P2P_server.cpp
@@ -5,10 +5,12 @@
 #include <unistd.h>
 #include <string.h>
 #include "../shared/P2P_shared.h"
+#include <sys/epoll.h>
 
 #define PORT 8080
 #define BUFF_SIZE 4024
-
+#define EPOLL_QUEUE_LEN 32
+#define TIMEOUT 30
 int main(int argc, char* argv[]){
 
     int server_fd, new_socket, valread;
@@ -17,6 +19,15 @@ int main(int argc, char* argv[]){
     int addrlen = sizeof(address);
     char buffer[BUFF_SIZE] = {0};
     char *hello = "Hello from server at ";
+
+
+    //Create epoll call FD
+    uint16_t epfd = epoll_create1(0);
+
+    if(epfd == -1){
+        fprintf(stderr, "Creating epoll fd failed\n");
+        exit(EXIT_FAILURE);
+    }
 
     //Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM,0)) == 0){
@@ -40,22 +51,74 @@ int main(int argc, char* argv[]){
         exit(EXIT_FAILURE);
     }
 
-    while(true){
-        //Start to listen
-        if(listen(server_fd, 3)<0){
-            perror("Listen failed");
-            exit(EXIT_FAILURE);
-        }
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){
-            perror("Accept failed");
-            exit(EXIT_FAILURE);
-        }
-        valread=read(new_socket,buffer,BUFF_SIZE);
-        printf("%s\n",buffer);
-        send(new_socket,hello,strlen(hello),0);
-        printf("Hello Message Sent\n");
+    //Start to listen
+    if(listen(server_fd, 3)<0){
+        perror("Listen failed");
+        exit(EXIT_FAILURE);
     }
 
+    //Register socket fd to the epoll
+    struct epoll_event event;    
+    //Set some flags on events to detect, read avail, connection close, and edge-trigger notification  
+    event.events = EPOLLIN|EPOLLRDHUP|EPOLLET;
+    event.data.fd = server_fd;
+    if((epoll_ctl, epfd, EPOLL_CTL_ADD, server_fd, &event) == -1){
+        perror("Error in epoll_ctl");
+        exit(EXIT_FAILURE);
+    }
+
+
+    while(true){
+
+        //Start monitoring epoll for connections
+        struct epoll_event events[EPOLL_QUEUE_LEN];
+        int n = epoll_wait(epfd,events,EPOLL_QUEUE_LEN,TIMEOUT);
+
+        //Go through all events
+        for(n;n>0;n--){
+            if(events[n].events & EPOLLRDHUP){
+                printf("Client socket closed\n");
+                //If something needed to be done here do it
+            }else if(events[n].events & EPOLLIN){
+                if(events[n].data.fd == server_fd) {
+                    printf("Connection accept");
+                    
+                    //Accept connection
+                    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,(socklen_t*)&addrlen))<0){
+                        perror("Accept failed");
+                        exit(EXIT_FAILURE);
+                    }     
+                    //Register FD
+                    struct epoll_event new_ev;
+                    new_ev.events = EPOLLIN|EPOLLRDHUP|EPOLLET;
+                    new_ev.data.fd=new_socket;
+                    if((epoll_ctl, epfd, EPOLL_CTL_ADD, new_socket, &new_ev) == -1){
+                        perror("Error in epoll_ctl");
+                        exit(EXIT_FAILURE);
+                    }                    
+
+                }else{
+                    char* read_buff[BUFF_SIZE];
+                    printf("Get Data");
+                    valread=read(events[n].data.fd,read_buff,BUFF_SIZE);
+                    printf("%s\n",read_buff);
+                    send(events[n].data.fd,hello,strlen(hello),0);
+                    printf("Hello Message Sent\n");                    
+
+                }
+            }
+        }
+
+
+
+    }
+
+
+    //close epoll
+    if(close(epfd)){
+        fprintf(stderr,"Failed to close epoll fd\n");
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 
